11365958da78233a192cd5c9555b89e775a0ebb3
Elliot Nicolás <A00840918@tec.mx>
2025-09-18 10:57:20 -0600

    Update memory.py

--- memory.py
+++ memory.py
@@ -1,4 +1,5 @@
-from random import *
+import random
+#from random import *
 from turtle import *
 from freegames import path
 
@@ -39,12 +40,16 @@ def tap(x, y):
         hide[mark] = False
         state['mark'] = None
 
+
 def draw():
     "Draw image and tiles."
     clear()
     goto(0, 0)
     shape(car)
     stamp()
+    # Mariana Guerrero Pérez - A00840918
+    #Colores como innovación
+    colors = ["#FF5733", "#33FF57", "#3357FF", "#F1C40F", "#9B59B6", "#E67E22","#2ECC71", "#1ABC9C", "#3498DB", "#E74C3C", "#D35400", "#7D3C98","#16A085", "#2980B9", "#8E44AD", "#27AE60", "#C0392B", "#F39C12","#D81B60", "#5DADE2", "#52BE80", "#AF7AC5", "#CA6F1E", "#45B39D","#1F618D", "#884EA0", "#229954", "#B03A2E", "#7B241C", "#117A65","#2471A3", "#616A6B"]
 
     for count in range(64):
         if hide[count]:
@@ -53,17 +58,20 @@ def draw():
 
     mark = state['mark']
 
+    num_color = {i: colors[i] for i in range(32)} #color escogido por tiles
+
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
         goto(x + 2, y)
-        color('black')
+        #color("black")
+        color(num_color[tiles[mark]]) #escoger por num
         write(tiles[mark], font=('Arial', 30, 'normal'))
 
     update()
     ontimer(draw, 100)
 
-shuffle(tiles)
+random.shuffle(tiles)
 setup(420, 420, 370, 0)
 addshape(car)
 hideturtle()
